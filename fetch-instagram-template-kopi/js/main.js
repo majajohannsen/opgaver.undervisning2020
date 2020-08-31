9dcd3fc6a51e19d4b5364cc5babb9d2f64e32ff0
majajohannsen <johannsen12@hotmail.com>
2020-08-31 09:55:20 +0200

    version 2

--- fetch-instagram-template-kopi/js/main.js
+++ fetch-instagram-template-kopi/js/main.js
@@ -9,16 +9,35 @@ function fetchInstagramPosts(userName) {
     })
     .then(function (data) {
       console.log(data);
-      // let posts = data.graphql.user.edge_owner_to_timeline_media.edges;
-      // TODO: call appendPosts with ig posts
+      let posts = data.graphql.user.edge_owner_to_timeline_media.edges;
+      console.log(posts);
+      appendPosts(posts);
     });
 }
 
 function appendPosts(posts) {
   // TODO: Append posts using a loop
+
+  let htmlTemplate = "";
+
+  for (const post of posts) {
+console.log(posts);
+let imageUrl = post.node.thumbnail_src;
+let likes = post.node.edge_liked_by.count;
+let caption = post.node.edge_media_to_caption.edges[0].node.text;
+
+htmlTemplate += /*html*/`
+<article>
+<img src="${imageUrl}">
+<p>Likes: ${likes}</p>
+<p>${caption}</p>
+  </article>
+`;
+  }
+  document.querySelector("#instagram-posts").innerHTML = htmlTemplate;
 }
 
-function appendProfileInfo(userInfo) {
+function appendProfileInfo() {
   // TODO Append IG profile info like username, full name, mail, biography, profile picture, etc. 
 }
 
