d4100bfad6ec39cbdee4c467f738a8fe2c699f6d
majajohannsen <johannsen12@hotmail.com>
2020-09-08 10:25:23 +0200

    version 1223

--- headless-cms-template-kopi/js/main.js
+++ headless-cms-template-kopi/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const url = "https://api.cederdorff.com/wp-json/wp/v2/posts";
+const url = "http://wordpress.majajohannsen.dk/wp-json/wp/v2/posts";
 // const url = "https://api.cederdorff.com/wp-json/wp/v2/posts?_embed";
 
 fetch(url)
@@ -9,12 +9,25 @@ fetch(url)
   })
   .then(function (posts) {
     console.log(posts);
+    appendPosts(posts);
   });
 
 // append wp posts to the DOM
 function appendPosts(posts) {
+let template ="";
 
-}
+for (const post of posts) {
+  template += /*html*/ `
+      <article>
+            <img src="${post.acf.image.url}">
+            <h2>${post.title.rendered}</h2>
+            <a href="mailto:${post.acf.mail}">${post.acf.mail}</a>
+            <p>${post.acf.age} years old</p>
+  </article>
+  `; 
+} 
+ document.getElementById("content").innerHTML = template;
+ }
 
 // get the featured image url
 function getFeaturedImageUrl(post) {
