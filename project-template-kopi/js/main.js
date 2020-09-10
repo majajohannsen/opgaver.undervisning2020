3eb6183d9b1d754d850e082f1a05fe6113716c30
majajohannsen <johannsen12@hotmail.com>
2020-09-10 10:21:33 +0200

    version 444

--- project-template-kopi/js/main.js
+++ project-template-kopi/js/main.js
@@ -5,16 +5,54 @@ constructor(name, initials) {
   this.initials = initials;
   this.department = department;
   this.img = img;
+  this.age = age;
 }
 
 getMail(){
-  let mail = `${this.initials}@eaaa.dk`;
-  return mail;
+  return `${this.initials}@eaaa.dk`;
 }
 
+log(){
+let template = `
+Name: ${this.name}
+Initials: ${this.initials}
+Mail: ${this.getMail()}
+Department: ${this.department}
+Image Url: ${this.img}
+`;
 }
 
-let teacher1 = new Teacher ("Birgitte", "bki", "Mulitimediedesign", "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg");
+sayHi (){
+    alert(`Hi ${this.name}`);
+  }
+
+  append(containerId){
+    let tempale = /*html*/`
+    <article>
+      <h2> ${this.name}</h2>
+      <p><a href ="mailto:${this.getMail()}">${this.getMail()}</a></p>
+  </article>
+      `;
+      document.querySelector(`#${containerId}`).innerHTML += template;
+  }
+}
+
+
+let teacher1 = new Teacher ("Birgitte", "bki", "Mulitimediedesign", "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg", "65 years old");
 console.log(teacher1);
 teacher1.department = "Web Development";
 console.log(teacher1.getMail());
+
+let teacher2 = new Teacher ("Rasmus", "race", "Mulitimediedesign", "https://cederdorff.com/img/rasmus.jpg", "30 years old");
+console.log(teacher2);
+teacher2.department = "Web Development";
+console.log(teacher2.getMail());
+
+let teacher3 = new Teacher ("Martin", "race", "Mulitimediedesign", "https://cederdorff.com/img/rasmus.jpg", "30 years old");
+console.log(teacher3);
+teacher3.department = "Web Development";
+console.log(teacher3.getMail());
+
+
+console.log(teacher1)
+console.log()
