8aa2fd9b771ccbb46d4c5dedf43b647a5a5dadbe
majajohannsen <johannsen12@hotmail.com>
2020-09-10 09:47:25 +0200

    version 8

--- project-template-kopi/js/main.js
+++ project-template-kopi/js/main.js
@@ -1,33 +1,20 @@
-"use strict"; // to enable strict mode and modern JavaScript functionality
+class Teacher {
 
-// appends a string to the DOM
-document.querySelector("#content").innerHTML = "Hi Frontenders!";
-
-function showAlert() {
-  alert('Open your Developer Console!');
+constructor(name, initials) {
+  this.name = name;
+  this.initials = initials;
+  this.department = department;
+  this.img = img;
 }
 
-// declaring teacher objects
-let teacher1 = {
-  name: 'Birgitte',
-  initials: 'bki'
-};
+getMail(){
+  let mail = `${this.initials}@eaaa.dk`;
+  return mail;
+}
 
-let teacher2 = {
-  name: 'Rasmus',
-  initials: 'race'
-};
+}
 
-// let teacher3 = {
-  
-// };
-
-// let teacher4 = {
-
-// };
-
-// logs the teacher objects to the console
+let teacher1 = new Teacher ("Birgitte", "bki", "Mulitimediedesign", "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg");
 console.log(teacher1);
-console.log(teacher2);
-// console.log(teacher3);
-// console.log(teacher4);
+teacher1.department = "Web Development";
+console.log(teacher1.getMail());
