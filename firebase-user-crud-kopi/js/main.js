4caa76bbc884c46661ed05b907f29e4a10f60c30
majajohannsen <johannsen12@hotmail.com>
2020-09-11 09:24:11 +0200

    version 343434

--- firebase-user-crud-kopi/js/main.js
+++ firebase-user-crud-kopi/js/main.js
@@ -1,14 +1,15 @@
 "use strict";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCI_VTBj8inrJWIjIYf_Y7bBpT9aRRQS1o",
-  authDomain: "user-app-289f1.firebaseapp.com",
-  databaseURL: "https://user-app-289f1.firebaseio.com",
-  projectId: "user-app-289f1",
-  storageBucket: "user-app-289f1.appspot.com",
-  messagingSenderId: "438369021654",
-  appId: "1:438369021654:web:8138ce7351d51603c0a377"
+var firebaseConfig = {
+  apiKey: "AIzaSyBe0Lk3qwX3e71JBYw4VP-wHfJc8AWol4g",
+  authDomain: "fir-2020-70d61.firebaseapp.com",
+  databaseURL: "https://fir-2020-70d61.firebaseio.com",
+  projectId: "fir-2020-70d61",
+  storageBucket: "fir-2020-70d61.appspot.com",
+  messagingSenderId: "947284176349",
+  appId: "1:947284176349:web:53aa160a8b7029dab91a34",
+  measurementId: "G-F2CGPN25HW"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
