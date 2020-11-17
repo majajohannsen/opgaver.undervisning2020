452cfc406b9d981747f9de2651d102f7ed9d0bc0
majajohannsen <johannsen12@hotmail.com>
2020-11-17 08:44:18 +0100

    version 111

---
+++ project-template/js/main.js
@@ -0,0 +1,34 @@
+"use strict"; // to enable strict mode and modern JavaScript functionality
+
+// appends a string to the DOM
+document.querySelector("#content").innerHTML = "Hi Frontenders!";
+
+function showAlert() {
+  alert('Open your Developer Console!');
+}
+
+// declaring teacher objects
+let teacher1 = {
+  name: 'Birgitte',
+  initials: 'bki'
+};
+
+let teacher2 = {
+  name: 'Rasmus',
+  initials: 'race'
+  
+};
+
+// let teacher3 = {
+  
+// };
+
+// let teacher4 = {
+
+// };
+
+// logs the teacher objects to the console
+console.log(teacher1);
+console.log(teacher2);
+// console.log(teacher3);
+// console.log(teacher4);
